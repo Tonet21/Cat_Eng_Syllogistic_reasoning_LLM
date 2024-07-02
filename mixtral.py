5b7226af34d49df592ff1f07603bffb43dd0e107
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-07-02 10:57:36 +0200

    Add files via upload

---
+++ mixtral.py
@@ -0,0 +1,25 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from prompt import prompts
+
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1", device_map="auto")
+
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
+
+model_conclusions = []
+
+
+for prompt in prompts:
+
+    messages = [
+
+    {"role": "user", "content": prompt},
+    ]
+
+    model_inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
+
+    generated_ids = model.generate(model_inputs, max_new_tokens=100, do_sample=True)
+
+    model_conclusion = tokenizer.batch_decode(generated_ids)[0]
+
+    model_conclusions.append(model_conclusion)
+
---
+++ syllo.py
@@ -0,0 +1,123 @@
+from collections import OrderedDict
+import random
+
+
+
+
+
+sylloExamples = [('actuaries', 'sculptors', 'writers'), ('assistants', 'poets', 'scientists'),
+                 ('athletes', 'assistants', 'chefs'), ('chemists', 'drivers', 'dancers'), 
+                 ('chemists', 'workers', 'painters'), ('clerks', 'butchers', 'athletes'), 
+                 ('dancers', 'bankers', 'riders'), ('doctors', 'riders', 'investors'), 
+                 ('drivers', 'porters', 'chemists'), ('farmers', 'surfers', 'writers'),
+                 ('gamblers', 'cleaners', 'models'), ('golfers', 'cyclists', 'assistants'),
+                 ('hunters', 'analysts', 'swimmers'), ('joggers', 'actors', 'carpenters'),
+                 ('linguists', 'cooks', 'models'), ('linguists', 'skaters', 'singers'),
+                 ('managers', 'clerks', 'butchers'), ('miners', 'tellers', 'poets'), 
+                 ('models', 'tailors', 'florists'), ('nurses', 'scholars', 'buyers'),
+                 ('planners', 'sailors', 'engineers'), ('riders', 'agents', 'waiters'),
+                 ('riders', 'novelists', 'linguists'), ('runners', 'opticians', 'clerks'),
+                 ('scientists', 'novelists', 'florists'), ('skaters', 'barbers', 'cooks'),
+                 ('students', 'cashiers', 'doctors'), ('students', 'hikers', 'designers'),
+                 ('surfers', 'painters', 'porters'), ('therapists', 'hikers', 'opticians')]
+
+
+wordType = {"S": "subject",
+            "M": "middle",
+            "P": "predicate"}
+
+
+class Figure:
+    def __init__(self, type=None):
+        self.figureType = type
+        if type == 1:
+            self.major = ["M", "P"]
+            self.minor = ["S", "M"]
+
+        elif type == 2:
+            self.major = ["P", "M"]
+            self.minor = ["S", "M"]
+
+        elif type == 3:
+            self.major = ["M", "P"]
+            self.minor = ["M", "S"]
+
+        elif type == 4:
+            self.major = ["P", "M"]
+            self.minor = ["M", "S"]
+
+
+class Mood:
+    linkMapping = {"A": "all ",
+                   "E": "no ",
+                   "I": "some ",
+                   "O": "some ",
+    }
+    postVerbLink = {"A": "",
+                    "E": "",
+                    "I": "",
+                    "O": "not "}
+
+    def __init__(self, links):
+        self.links = links
+        self.majorLink = links[0]
+        self.minorLink = links[1]
+
+class Syllogism:
+    def __init__(self, figure, mood, syllowords):
+        self.figure = Figure(figure)
+        self.mood = Mood(mood)
+        self.subject = syllowords[0]
+        self.middle = syllowords[1]
+        self.predicate = syllowords[2]
+
+    def generate(self):
+        allSentences = []
+        for sentence in ("major", "minor"):
+            sOut = ""
+            linkage = getattr(self.mood, sentence + "Link")
+            linkWord = self.mood.linkMapping[linkage]
+            linkWord = linkWord.capitalize()
+            sOut += linkWord
+            sFormat = getattr(self.figure, sentence)
+            wt = wordType[sFormat[0]]
+            sOut += getattr(self, wt)
+            sOut += " are "
+            sOut += self.mood.postVerbLink[linkage]
+            wt = wordType[sFormat[1]]
+            sOut += getattr(self, wt)
+
+            allSentences.append(sOut)
+        return allSentences
+
+def generateAll():
+    all_generated_syllogisms = []
+    possible_conclusions = []
+    for terms in sylloExamples:
+        for i in range(1, 5):
+            for a in "AEIO":
+                for b in "AEIO":         
+                         s = Syllogism(i, a + b, terms)
+                         syllogism = s.generate() + [a + b]
+                         all_generated_syllogisms.append(syllogism)
+                         choices = [f"""All {terms[0]} are {terms[2]}""",
+                          f"""No {terms[0]} are {terms[2]}""",
+                          f"""Some {terms[0]} are {terms[2]}""",
+                          f"""Some {terms[0]} are not {terms[2]}""" ]
+                         possible_conclusions.append(choices)
+
+
+
+    return all_generated_syllogisms, possible_conclusions
+
+syllogisms = generateAll()[0]
+possible_conclusions = generateAll()[1] 
+
+print(syllogisms[0])
+print(possible_conclusions[0])
+
+
+
+
+
+
