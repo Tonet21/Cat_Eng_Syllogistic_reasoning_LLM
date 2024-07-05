8c938cfbff7df42a49fd0dca2fa75850be6da2d4
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-07-05 09:53:09 +0200

    Add files via upload

---
+++ cat_prompt.py
@@ -0,0 +1,26 @@
+from cat_syllo import syllogisms, possible_conclusions
+
+
+prompts = []
+i = 0
+for premise1, premise2, mood in syllogisms:
+        prompt = f""" 
+    La teva tasca és triar la conclusió vàlida d'un sil·logisme. Se't presentarà una llista 
+    amb quarte conclusions possibles. A part se't presentaran les premisses del sil·logisme 
+    entre tres accents oberts. La teva resposta serà únicament una de les conclusions de la llista 
+    o <NVC> si no trobes una coclusió vàlida. El sil·logisme se't presentarà en el següent format:
+    Premisa 1: <premisa 1>
+    Premisa 2: <premisa 2>
+    Opcions: <llista de conclusions possibles separades per comes>
+
+    Pren-te el teu temps per pensar, fes-ho sense pressa..
+
+    ``` 
+    Premisa 1: {premise1}
+    Premisa 2: {premise2}
+    Opcions: {possible_conclusions[i][0]}, {possible_conclusions[i][1]}, {possible_conclusions[i][2]}, {possible_conclusions[i][3]}
+    ```
+
+    """
+        prompts.append(prompt)
+        i += 1
---
+++ cat_syllo.py
@@ -0,0 +1,112 @@
+sylloExamples = [('actuaries', 'sculptors', 'writers'), ('assistants', 'poets', 'scientists'),
+                 ('athletes', 'assistants', 'chefs'), ('chemists', 'drivers', 'dancers'), 
+                 ('chemists', 'workers', 'painters'), ('clerks', 'butchers', 'athletes'), 
+                 ('dancers', 'bankers', 'riders'), ('doctors', 'riders', 'investors'), 
+                 ('drivers', 'porters', 'chemists'), ('farmers', 'surfers', 'writers'),
+                 ('gamblers', 'cleaners', 'models'), ('golfers', 'cyclists', 'assistants'),
+                 ('hunters', 'analysts', 'swimmers'), ('joggers', 'actors', 'carpenters'),
+                 ('linguists', 'cooks', 'models'), ('linguists', 'skaters', 'singers'),
+                 ('managers', 'clerks', 'butchers'), ('miners', 'tellers', 'poets'), 
+                 ('models', 'tailors', 'florists'), ('nurses', 'scholars', 'buyers'),
+                 ('planners', 'sailors', 'engineers'), ('riders', 'agents', 'waiters'),
+                 ('riders', 'novelists', 'linguists'), ('runners', 'opticians', 'clerks'),
+                 ('scientists', 'novelists', 'florists'), ('skaters', 'barbers', 'cooks'),
+                 ('students', 'cashiers', 'doctors'), ('students', 'hikers', 'designers'),
+                 ('surfers', 'painters', 'porters'), ('therapists', 'hikers', 'opticians')]
+
+
+wordType = {"S": "subject",
+            "M": "middle",
+            "P": "predicate"}
+
+
+class Figure:
+    def __init__(self, type=None):
+        self.figureType = type
+        if type == 1:
+            self.major = ["M", "P"]
+            self.minor = ["S", "M"]
+
+        elif type == 2:
+            self.major = ["P", "M"]
+            self.minor = ["S", "M"]
+
+        elif type == 3:
+            self.major = ["M", "P"]
+            self.minor = ["M", "S"]
+
+        elif type == 4:
+            self.major = ["P", "M"]
+            self.minor = ["M", "S"]
+
+
+class Mood:
+    linkMapping = {"A": "Tots els ",
+                   "E": "No tots els ",
+                   "I": "Alguns ",
+                   "O": "Alguns ",
+    }
+    postVerbLink = {"A": "",
+                    "E": "",
+                    "I": "",
+                    "O": " no"}
+
+    def __init__(self, links):
+        self.links = links
+        self.majorLink = links[0]
+        self.minorLink = links[1]
+
+class Syllogism:
+    def __init__(self, figure, mood, syllowords):
+        self.figure = Figure(figure)
+        self.mood = Mood(mood)
+        self.subject = syllowords[0]
+        self.middle = syllowords[1]
+        self.predicate = syllowords[2]
+
+
+    def generate(self):
+        allSentences = []
+        for sentence in ("major", "minor"):
+            sOut = ""
+            linkage = getattr(self.mood, sentence + "Link")
+            linkWord = self.mood.linkMapping[linkage]
+            linkWord = linkWord.capitalize()
+            sOut += linkWord
+            sFormat = getattr(self.figure, sentence)
+            wt = wordType[sFormat[0]]
+            sOut += getattr(self, wt)
+            sOut += self.mood.postVerbLink[linkage]
+            sOut += " són "
+            wt = wordType[sFormat[1]]
+            sOut += getattr(self, wt)
+
+            allSentences.append(sOut)
+        return allSentences
+
+def generateAll():
+    all_generated_syllogisms = []
+    possible_conclusions = []
+    for terms in sylloExamples:
+        for i in range(1, 5):
+            for a in "AEIO":
+                for b in "AEIO":         
+                         s = Syllogism(i, a + b, terms)
+                         syllogism = s.generate() + [a + b]
+                         all_generated_syllogisms.append(syllogism)
+                         choices = [f"""Tots {terms[0]} són {terms[2]}""",
+                          f"""No tots els {terms[0]} són {terms[2]}""",
+                          f"""Alguns {terms[0]} són {terms[2]}""",
+                          f"""Alguns {terms[0]} no són {terms[2]}""" ]
+                         possible_conclusions.append(choices)
+
+
+
+    return all_generated_syllogisms, possible_conclusions
+
+syllogisms = generateAll()[0]
+possible_conclusions = generateAll()[1] 
+
+
+
+
