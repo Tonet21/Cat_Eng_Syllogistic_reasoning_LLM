2e2b7469fcc9c26925d06f86b58d82925ba643c3
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-08-04 09:27:46 +0200

    Update mock_test.py

--- mock_test.py
+++ mock_test.py
@@ -11,30 +11,37 @@ for choice in possible_conclusions:
 
 conclusions = []  
 i = 0
-for prem1, prem2,mood in syllogisms:
+for prem1, prem2,mood, type_ in syllogisms:
     
         conclusion = random.choice(possible_conclusions[i]) 
         if re.search("^Some", conclusion):
             if re.search(".+not", conclusion):
-                conclusion = [mood, conclusion, "O"]
+                conclusion = [mood, conclusion, "O", type_]
             else:
-                conclusion = [mood, conclusion, "I"]
+                conclusion = [mood, conclusion, "I", type_]
         elif re.search("^All", conclusion):
-            conclusion = [mood, conclusion, "A"]
+            conclusion = [mood, conclusion, "A", type_]
         elif re.search("^No", conclusion):
-            conclusion = [mood, conclusion, "E"]
+            conclusion = [mood, conclusion, "E", type_]
         else: 
-            conclusion = [mood, conclusion, "NVC"]
+            conclusion = [mood, conclusion, "NVC", type_]
     
         conclusions.append(conclusion)
         i += 1 
 
 
 conclusion_count = defaultdict(dict)
-for syllo_mood, _, conclusion_mood in conclusions:
+for syllo_mood, _, conclusion_mood, type_ in conclusions:
   conclusion_count[syllo_mood][conclusion_mood] = conclusion_count[syllo_mood].get(conclusion_mood, 0) + 1
 
 
+type_count = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+for syllo_mood, _, conclusion_mood, type_ in conclusions:
+    type_count[syllo_mood][type_][conclusion_mood] += 1
+
+
+print(type_count)
+##print(conclusion_count)
  
 data = conclusion_count
 columns = ["A", "E", "I", "O", "NVC"]
@@ -53,6 +60,25 @@ excel_file_path = "syllogism_conclusions.xlsx"
 df.to_excel(excel_file_path, index=False)
 
 print(f"DataFrame saved to {excel_file_path}")
+
+
+flattened_data = []
+for mood, types in type_count.items():
+    for type_, moods in types.items():
+        row = {"syllogism": mood, "type": type_, "A": 0, "E": 0, "I": 0, "O": 0, "NVC": 0}
+        for conclusion_mood, count in moods.items():
+            row[conclusion_mood] = count
+        flattened_data.append(row)
+
+# Create a DataFrame
+columns = ["syllogism", "type", "A", "E", "I", "O", "NVC"]
+df = pd.DataFrame(flattened_data, columns=columns)
+
+# Save the DataFrame to an Excel file
+excel_file_path = "syllogism_type_conclusions.xlsx"
+df.to_excel(excel_file_path, index=False)
+
+print(f"Excel file created at: {excel_file_path}")
 
 
 
