f3787995416f5a61d4d048bb0e6799a53a7ffc41
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2024-08-04 09:33:45 +0200

    Update and rename mixtral.py to mistral.py

---
+++ mistral.py
@@ -0,0 +1,44 @@
+from huggingface_hub import snapshot_download
+from pathlib import Path
+from mistral_inference.model import Transformer
+from mistral_inference.generate import generate
+from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+from mistral_common.protocol.instruct.messages import UserMessage
+from mistral_common.protocol.instruct.request import ChatCompletionRequest
+from prompt import prompts,system_message
+from mistral_common.protocol.instruct.messages import SystemMessage
+
+
+access_token = ""
+
+local_model_dir = "/mnt/cimec-storage6/users/antoni.llinasbarbara/Cat_Eng_Syllogistic_reasoning_LLM/models"
+mistral_models_path = Path(local_model_dir)
+mistral_models_path.mkdir(parents=True, exist_ok=True)
+
+snapshot_download(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.3",
+    allow_patterns=["params.json", "consolidated.safetensors", "tokenizer.model.v3"],
+    local_dir=local_model_dir,
+    token=access_token
+)
+
+
+
+tokenizer = MistralTokenizer.from_file(f"{mistral_models_path}/tokenizer.model.v3")
+model = Transformer.from_folder(mistral_models_path)
+
+model_conclusions = []
+
+system_message = SystemMessage(content=system_message)
+
+for prompt in prompts:
+    completion_request = ChatCompletionRequest(messages=[system_message, UserMessage(content=prompt)])
+
+    tokens = tokenizer.encode_chat_completion(completion_request).tokens
+
+    out_tokens, _ = generate([tokens], model, max_tokens=64, temperature=0.0, eos_id=tokenizer.instruct_tokenizer.tokenizer.eos_id)
+    result = tokenizer.instruct_tokenizer.tokenizer.decode(out_tokens[0])
+    model_conclusions.append(result)
+
+
+
